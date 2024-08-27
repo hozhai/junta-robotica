aded302c49e8ecd55c192863d8b482a7ca825f97
Hongmeng Zhai <hongmengzhai@zhai-macos.local>
2024-08-27 13:48:12 -0400

    small progress

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,18 +2,22 @@ import Image from "next/image";
 
 export default function Navbar() {
     return (
-        <nav className="flex justify-evenly align-center fixed min-w-full">
-            <div className="flex justify-center items-center">
+        <nav className="flex justify-between items-center fixed min-w-full px-12">
+            <div className="">
+                <ul className="flex justify-center items-center list-none">
+                    <li>Home</li>
+                </ul>
+            </div>
+            <div className="flex justify-center items-center self-start">
                 <Image
                     src="https://dummyimage.com/500x500/000/fff&text=+LOGO+"
                     width={80}
                     height={80}
                     alt="Logo Placeholder Image"
                 />
-                <h3 className="mx-5 text-slate-300">FESRO</h3>
             </div>
             <div>
-                <ul>
+                <ul className="flex justify-center items-center list-none">
                     <li>Home</li>
                 </ul>
             </div>
