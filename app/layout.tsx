b95960310c28bebd3b195abd5d972d056c0fcd73
Hongmeng Zhai <hongmengzhai@zhai-macos.local>
2024-08-28 13:31:03 -0400

    added avatar icon to nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { GeistSans } from "geist/font/sans";
 
-import Navbar from "@/app/components/Navbar";
+import Navbar from "@/components/Navbar";
 import "./globals.css";
 
 const inter = Inter({
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
     return (
         <main className="grid items-center justify-start min-h-screen">
-            <div className="bg-blue-500 p-20">
+            <div className="p-20">
                 <h1 className="font-sans font-semibold text-7xl">
                     Primer Festival de Robòtica
                 </h1>
